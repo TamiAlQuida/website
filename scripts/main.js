1505d1c5cc41454353b3a813f6c2be0bcb3c86e2
TamiAlQuida <whozie@gmail.com>
2022-11-07 17:21:15 +0100

    weather app

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-pijpoijpoi
+let weather = {
+    apiKey: "9b3c2b3d48ef11aa93a76ef5db38bde6",
+    fetchWeather: function () {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+};
+
+https://www.youtube.com/watch?v=WZNG8UomjSI 20:29
