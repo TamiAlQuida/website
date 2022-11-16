4822d68b840c57335176407134461a7617290365
TamiAlQuida <whozie@gmail.com>
2022-11-16 16:22:40 +0100

    Fetch weather with ApiKey

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 let weather = {
-    apiKey: "9b3c2b3d48ef11aa93a76ef5db38bde6",
-    fetchWeather: function () {
+    apiKey: "db72b8114fbd9c9d4954d31f16950834",
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
         )
             .then((response) => response.json())
             .then((data) => console.log(data));
     },
+    displayWeather: function (data) {
+
+    }
 };
 
-//https://www.youtube.com/watch?v=WZNG8UomjSI 20: 29 
+//https://www.youtube.com/watch?v=WZNG8UomjSI 22: 08 
