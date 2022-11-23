b9266203bfddc6a2b77feae511a6b9601245a2f0
TamiAlQuida <whozie@gmail.com>
2022-11-23 14:49:54 +0100

    check in

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed)
+    /*console.log(name, icon, description, temp, humidity, speed)*/
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".temp").innerText = temp + "°C";
@@ -40,4 +40,4 @@ document.querySelector(".search_bar").addEventListener("keyup", function (event)
   }
 });
 
-//https://www.youtube.com/watch?v=WZNG8UomjSI 26: 16 
+//https://www.youtube.com/watch?v=WZNG8UomjSI
