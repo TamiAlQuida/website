320e200a8ef2cd809d55dc0dc0abc125c1074592
TamiAlQuida <whozie@gmail.com>
2022-12-22 16:39:56 +0100

    Fixed display names at start up

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-const API_KEY = process.env.API_KEY_OPEN_WEATHER;
+/*const API_KEY = process.env.API_KEY_OPEN_WEATHER;*/
 
 let weather = {
-  /*apiKey: "db72b8114fbd9c9d4954d31f16950834",*/
-  apiKey: API_KEY,
+  apiKey: "db72b8114fbd9c9d4954d31f16950834",
+  /*apiKey: API_KEY,*/
   /*apiKey: process.env.API_KEY,*/
   fetchWeather: function (city) {
     fetch(
